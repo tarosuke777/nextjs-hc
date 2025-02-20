13b7044bce2f10d9c2af610e65334967fcc521e5
tarosuke <felis.silvestris.catus.7777@gmail.com>
2025-02-20 12:29:22 +0900

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,128 @@
-import Image from "next/image";
+'use client'
+
+import {useRef, useEffect} from "react";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+	const socketRef = useRef<WebSocket | null>(null);
+	const contentRef = useRef<HTMLInputElement>(null);
+	const channelIdRef = useRef<String>('0');
+
+	useEffect(() => {
+
+		const messagesTable = document.getElementById('jsiConversation')!.getElementsByTagName('tbody')[0];
+
+		const urlParams = new URLSearchParams(window.location.search);
+		let channelId = urlParams.get('channelId');
+		
+		if (channelId === null) {
+		  channelId = '1';
+		}
+		
+		channelIdRef.current = channelId;
+
+		fetch(`http://localhost:8080/messages?channelId=${channelId}`, {
+		  method: 'GET',
+		  headers: {
+		    'Accept': 'application/json'
+		  }
+		})
+		.then(response => {
+		  if (!response.ok) {
+		    throw new Error(`HTTP error! status: ${response.status}`);
+		  }
+		  return response.json();
+		})
+		.then(data => {
+		  messagesTable.innerHTML = '';
+
+		  data.forEach((message : any) => { showMessage(message);});
+		})
+		.catch(error => {
+		  console.error('Error fetching messages:', error);
+		  alert('メッセージの取得に失敗しました。');
+		});
+		
+    
+		const websocket = new WebSocket('ws://localhost:8080/hc-websocket?1');
+		websocket.addEventListener("error", (event) => {
+		console.log("WebSocket error: ", event);
+		});
+
+		socketRef.current = websocket;
+
+		const onMessage = (message : any) => {
+			  showMessage(JSON.parse(message.data));
+		};
+		
+		websocket.addEventListener('message', onMessage);
+		
+		return () => {
+		  websocket.close()
+		  websocket.removeEventListener('message', onMessage);
+		}
+	}, []);
+	
+	const showMessage = (message : any) => {
+		
+		const messagesTable = document.getElementById('jsiConversation')!.getElementsByTagName('tbody')[0];
+		const row = messagesTable.insertRow();
+		const createdAtCell = row.insertCell();
+		const messageCell = row.insertCell();
+
+		createdAtCell.textContent = message.createdAt;
+		messageCell.textContent = message.content;
+		createdAtCell.className = 'px-6 py-4';
+		messageCell.className = 'px-6 py-4';
+
+	}
+	
+	// https://flowbite.com/docs/forms/input-field/
+	// https://tailwindcss.com/docs/background-color
+
+	const sendName = () => {
+
+		if (!contentRef.current) return;
+
+		const msg = {
+		  content: contentRef.current.value,
+		  channelId: channelIdRef.current
+		};
+		
+		if (socketRef.current && socketRef.current.readyState === WebSocket.OPEN) { 
+			socketRef.current.send(JSON.stringify(msg));
+			contentRef.current.value = "";
+		} else {
+			console.error("WebSocket is not open. Cannot send message.");
+		}
+	}
+
+  return (
+		<div>
+			<div>
+				<table id="jsiConversation" className="text-left text-sm">
+					<thead className="text-xs uppercase">
+						<tr>
+							<th className="px-6 py-3">
+								Time
+							</th>
+							<th className="px-6 py-3">
+								Message
+							</th>
+						</tr>
+					</thead>
+					<tbody id="messages"></tbody>
+				</table>
+			</div>
+			<hr/>
+			<div className="grid grid-cols-2 gap-4 my-5 ml-3">
+			    <div>
+			        <input type="text" ref={contentRef} className="px-5 py-2.5 rounded-lg bg-gray-700 text-sm w-full"/>
+			    </div>
+				<div>
+					<button onClick={sendName} className="px-5 py-2.5 rounded-lg bg-gray-700 text-sm">Send</button>
+				</div>
+			</div>
+		</div>
   );
 }
