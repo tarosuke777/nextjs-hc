5f227800aeb9fed7c1eb959458e4cbaa36676e0f
tarosuke <felis.silvestris.catus.7777@gmail.com>
2025-06-14 17:22:55 +0900

    change env

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,32 @@
 import type { NextConfig } from "next";
+import localConfig from "./config/local.json";
+import awsConfig from "./config/aws.json";
+import prodConfig from "./config/prod.json";
+
+const appEnv = process.env.APP_ENV || "mock";
+
+let selectedConfig: Record<string, string>;
+
+switch (appEnv) {
+  case "local":
+    selectedConfig = localConfig;
+    break;
+  case "aws":
+    selectedConfig = awsConfig;
+    break;
+  case "prod":
+    selectedConfig = prodConfig;
+    break;
+  default:
+    selectedConfig = {};
+    break;
+}
 
 const nextConfig: NextConfig = {
+  env: {
+    APP_ENV: appEnv,
+    ...selectedConfig,
+  },
   output: "export",
   images: {
     unoptimized: true,
